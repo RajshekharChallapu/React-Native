4305111f23a72aa86629a3efb02897517d155afc
CR <rajshekhar@protonmail.com>
2018-03-20 12:00:25 -0500

    Styling a Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,21 @@
-// Import libraries for making a Component
+//Import libraries for making a Component
 import React from 'react';
 import { Text } from 'react-native';
 
 
 //Make a Component
 const Header = () =>{
-  return <Text>Albums!</Text>;
+  const { textStyle }= styles;
+
+  return <Text style={ textStyle }>Albums!</Text>;
 };
 
+//styling
+const styles={
+textStyle: {
+  fontSize: 20
+}
 
+}
 //Make the Component available to other parts of the App
 export default Header;
