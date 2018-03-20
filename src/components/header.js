3f3a85c2f385c3101bee91fa44047c48f9343ab0
CR <rajshekhar@protonmail.com>
2018-03-20 12:23:38 -0500

    styling properties added

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,35 @@
 //Import libraries for making a Component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 //Make a Component
 const Header = () =>{
-  const { textStyle }= styles;
+  const { textStyle, viewStyle }= styles;
 
-  return <Text style={ textStyle }>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+    <Text style={ textStyle }>Albums!</Text>
+</View>
+);
 };
-
 //styling
 const styles={
+  viewStyle:{
+    backgroundColor: '#F8F8F8',
+    justifyContent:'center',
+    alignItems:'center',
+    height:60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width:0, height:2},
+    shadowOpacity:0.9,
+    elevation: 2,
+    position:'relative'
+  },
 textStyle: {
   fontSize: 20
 }
-
 }
 //Make the Component available to other parts of the App
 export default Header;
