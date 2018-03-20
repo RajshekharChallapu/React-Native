18071abe278ae8e9d1c98ca3ea638854a9263602
CR <rajshekhar@protonmail.com>
2018-03-20 13:27:58 -0500

    List component Boilerplate

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 
 // Create a Component
 const App = () => (
   //if no text intended to write in import component ..that must be self closing ...eslint
-    <Header headerText= {'Albums'}/>
-);
 
+<View>
+    <Header headerText= {'Albums'}/>
+  <AlbumList />
+</View>
+);
 
 
 //Render it to the device
