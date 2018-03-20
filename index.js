eca34b954edb28f198ecf670c0bc30dfb61441a8
CR <rajshekhar@protonmail.com>
2018-03-20 11:50:56 -0500

    Component creation and import Header component to ROOT App component

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Import a library to help create a Component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+
+// Create a Component
+
+const App = () => (
+  //if no text intended to write in import component ..that must be self closing ...eslint
+    <Header/>
+);
+
+
+
+//Render it to the device
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// Import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+
+//Make a Component
+const Header = () =>{
+  return <Text>Albums!</Text>;
+};
+
+
+//Make the Component available to other parts of the App
+export default Header;
