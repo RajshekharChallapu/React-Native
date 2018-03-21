b96c6842b46843e70e68cbc562454da8a2bb5e30
CR <rajshekhar@protonmail.com>
2018-03-20 20:37:53 -0500

    component level state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,25 @@
 
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
+
 
 class AlbumList extends Component{
+
+  //STATE//used to update internal data of component..when event triggers and record and respond
+  state = { albums: [] };
+
+  //class component, it knows when to fetch data and get data to serve to UI by using
+  //lifecycle methods  ComponentWillMount,
+  ComponentWillMount(){
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    //promise
+    .then(response => this.setState({albums: response.data}));
+
+  }
 //render method ..we write class base components when  to fetch data and any helper method needed for componennt
 render() {
+  console.log(this.state);
   return (
     <View>
     <Text>Album List!!!</Text>
